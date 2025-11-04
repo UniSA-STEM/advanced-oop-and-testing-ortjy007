38b2476f21df2be35d1e4884b5cfda92b33fef02
Jorge Ortega <jortegadv@gmail.com>
2025-11-04 17:56:57 +1100

    file data + id updated

--- staff.py
+++ staff.py
@@ -1,8 +1,8 @@
-'''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
-This is my own work as defined by the University's Academic Integrity Policy.
-''
+# File          : staff.py
+# Author        : Jorge Ortega
+# Email id      : ortjy007@mymail.unisa.edu.au
+# Description   : COMP 1048 advanced-oop-and-testing Assignment 2
+#
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
